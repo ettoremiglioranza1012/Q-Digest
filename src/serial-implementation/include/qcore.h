0ff2bca10deb900f1a8c43ddcf71a5c675ac1163
anthony-tricarico <tricarico672@gmail.com>
2025-11-05 21:07:10 +0100

    feat: 90% core implementation completed + simple test added

--- src/serial-implementation/include/qcore.h
+++ src/serial-implementation/include/qcore.h
@@ -1,7 +1,9 @@
 #ifndef QCORE
 #define QCORE
+#include <stdbool.h>
 #include <stdio.h>
 
+/* ======================== STRUCT DEFINITIONS ==================*/
 /* Declare QDigestNode, the building block of the Data Structure */
 struct QDigestNode {
   // initialize pointers to left, right and parent nodes
@@ -16,9 +18,35 @@ struct QDigestNode {
 struct QDigest {
   // pointer to the node representing the root
   struct QDigestNode *root;
-
   size_t num_nodes;
   size_t N, K;
   size_t num_inserts;
 };
+
+/* ================= FUNCTION PROTOTYPES =======================*/
+size_t log_2_ceil(size_t n);
+struct QDigestNode *create_node(size_t lower_bound, size_t upper_bound);
+struct QDigest *create_q(struct QDigestNode *root, size_t num_nodes, size_t N,
+                         size_t K, size_t num_inserts);
+struct QDigest *create_tmp_q(size_t K, size_t upper_bound);
+size_t node_and_sibling_count(struct QDigestNode *n);
+bool delete_node_if_needed(struct QDigest *q, struct QDigestNode *n, int level,
+                           int l_max);
+void compress(struct QDigest *q, struct QDigestNode *n, int level, int l_max,
+              size_t nDivk);
+void print_tree(struct QDigest *q);
+void swap_q(struct QDigest **q1, struct QDigest **q2);
+void compress_if_needed(struct QDigest *q);
+void expand_tree(struct QDigest *q, size_t upper_bound);
+void insert(struct QDigest *q, size_t key, unsigned int count,
+            bool try_compress);
+void insert_node(struct QDigest *q, const struct QDigestNode *n);
+void expand_tree(struct QDigest *q, size_t upper_bound);
+size_t postorder_by_rank(struct QDigestNode *n, size_t *curr_rank,
+                         size_t req_rank);
+void preorder_to_string(struct QDigestNode *n);
+size_t percentile(struct QDigest *q, double p);
+char *to_string(struct QDigest *q);
+void from_string(char *ser);
+void merge(struct QDigest *q1, const struct QDigest *q2);
 #endif
--- src/serial-implementation/src/test.c
+++ src/serial-implementation/src/test.c
@@ -1,4 +1,67 @@
-#include "../include/linked.h"
+#include "../include/dynamic_array.h"
 #include "../include/memory_utils.h"
 #include "../include/qcore.h"
-#include "../include/queue.h"
+#include <stddef.h>
+#include <string.h>
+
+void compare_percentiles(double p, const Array *a, struct QDigest *q) {
+  int ip = (p * 1000 + 9) / 10;
+  printf("%dth percentile: %lu v/s %lu\n", ip, a->data[(int)(p * a->size - 1)],
+         percentile(q, p));
+}
+
+void test_poisson_distribution(int n, int k, int seed) {
+  printf("<< test_poisson_distribution >>\n");
+  int number = 1;
+  int repeat = 1;
+  bool flipped = false;
+  Array *a = xmalloc(sizeof(Array));
+  init_array(a, 128);
+  for (; get_length(a) != n; ++number) {
+    for (int i = 0; i < repeat && get_length(a) != n; ++i) {
+      push_back(a, number);
+    }
+    if (get_length(a) <= n / 2) {
+      if (!flipped)
+        repeat += 3;
+    } else {
+      if (!flipped)
+        repeat += 3;
+      flipped = true;
+      repeat -= 3;
+    }
+    if (repeat < 1)
+      repeat = 2;
+  }
+
+  Array *b = malloc(sizeof(Array));
+  init_array(b, a->capacity);
+  memcpy(b->data, a->data, sizeof(DAItem) * a->size);
+  b->size = a->size;
+
+  // missing sort and random shuffle here
+
+  // use the default parameter of for the upper bound 1 declared in the c++
+  // constructor
+  struct QDigest *q = create_tmp_q(k, 1);
+  for (size_t i = 0; i < get_length(b); ++i) {
+    insert(q, b->data[i], 1, true);
+  }
+  compare_percentiles(0.01, a, q);
+  compare_percentiles(0.02, a, q);
+  compare_percentiles(0.03, a, q);
+  for (double d = 0.05; d < 1.0; d += 0.05) {
+    compare_percentiles(d, a, q);
+  }
+}
+
+#ifdef TEST
+int main(void) {
+  const int K = 100;
+  int seed = 377;
+  int N = 65535;
+
+  test_poisson_distribution(N, K, seed);
+  return 0;
+}
+#endif
