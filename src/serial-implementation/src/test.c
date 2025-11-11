d3a971eae8b378386da0907e126c2c18df052104
anthony-tricarico <tricarico672@gmail.com>
2025-11-11 20:07:31 +0100

    feat: completed test base

--- src/serial-implementation/src/test.c
+++ src/serial-implementation/src/test.c
@@ -2,6 +2,7 @@
 #include "../include/memory_utils.h"
 #include "../include/qcore.h"
 #include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
 /* This is a helper function that is used by the qsort function */
@@ -88,14 +89,117 @@ void test_poisson_distribution(int n, int k, int seed) {
   }
 }
 
+void test_geometric_distribution(int n, int k, int seed) {
+  printf("<< test_geometric_distribution >>\n");
+  int number = 1;
+  int repeat = 1;
+  bool flipped = false;
+  Array *a = xmalloc(sizeof(Array)); // allocate memory for Array
+  init_array(a, 128); // initialize the Array with an initial capacity
+  for (; get_length(a) != n;
+       number += 1, repeat *= 2) { // start loop to add numbers to dynamic array
+    for (int i = 0; i < repeat && get_length(a) != n; ++i) {
+      push_back(a, number); // add number to dynamic array
+    }
+  }
+
+  Array *b = xmalloc(sizeof(Array)); // allocate memory for array b
+  init_array(b, a->capacity); // initialize array of b making sure it retains
+                              // the same capacity of a
+  memcpy(b->data, a->data, sizeof(DAItem) * a->size); // copy the data in a to b
+  b->size = a->size;                                  // copy size as well
+
+  // missing sort and random shuffle
+  shuffle(b->data, b->size);
+
+  // use the default parameter of for the upper bound 1 declared in the c++
+  // constructor
+  // printf("DEBUG: Before creating tree\n");
+  struct QDigest *q = create_tmp_q(
+      k, 1); // create a QDigest structure using the shorter constructor
+  // printf("DEBUG: After creating tree\n");
+  for (size_t i = 0; i < get_length(b); ++i) {
+    insert(q, b->data[i], 1, true); // insert data inside the QDigest
+  }
+
+  /* Compare percentiles between true values and using the QDigest */
+  compare_percentiles(0.01, a, q);
+  compare_percentiles(0.02, a, q);
+  compare_percentiles(0.03, a, q);
+  for (double d = 0.05; d < 1.0; d += 0.05) {
+    compare_percentiles(d, a, q);
+  }
+}
+
+double compute_compression_ratio(struct QDigest *q) {
+  return (double)q->num_nodes / (double)q->N;
+}
+
+void test_random_distribution(int n, int k, int seed) {
+  printf("<< test_random_distribution >>\n");
+  srand(seed);
+  struct QDigest *q = create_tmp_q(k, 1);
+  Array *a = xmalloc(sizeof(Array));
+  init_array(a, 128);
+
+  for (int i = 0; i < n; ++i) {
+    int number = rand() % n;
+    push_back(a, number);
+    insert(q, a->data[i], 1, true);
+  }
+  qsort(a->data, a->size, sizeof(a->data[0]), comp);
+  for (double d = 0.05; d < 1.0; d += 0.05) {
+    compare_percentiles(d, a, q);
+  }
+}
+
+void test_merge(int n, int k, int seed) {
+  printf("<< test_merge >>\n");
+  srand(seed);
+  struct QDigest *q1 = create_tmp_q(k, 1);
+  struct QDigest *q2 = create_tmp_q(k, 1);
+
+  Array *a = xmalloc(sizeof(Array));
+  init_array(a, 128);
+
+  for (int i = 0; i < n * 2; ++i) {
+    int number = rand() % n;
+    push_back(a, number);
+    if (i < n) {
+      insert(q1, number, 1, true);
+    } else {
+      insert(q2, number, 1, true);
+    }
+  }
+
+  printf("[q1] # of elements: %lu\n[q1] Compression Ratio: %lf\n\
+  [q2] # of elements: %lu\n[q2] Compression Ratio: %lf\n",
+         q1->num_nodes, compute_compression_ratio(q1), q2->num_nodes,
+         compute_compression_ratio(q2));
+
+  printf("DEBUG: before merge\n");
+  merge(q1, q2);
+
+  printf("[combined] # elements: %lu\n[combined] Compression Ratio: %lf\n",
+         q1->num_nodes, compute_compression_ratio(q1));
+
+  qsort(a->data, a->size, sizeof(a->data[0]), comp);
+  for (double d = 0.05; d < 1.0; d += 0.05) {
+    compare_percentiles(d, a, q1);
+  }
+}
+
 #ifdef TESTALL
 int main(void) {
   // higher values of K -> higher accuracy, lesser compression
-  const int K = 5;
+  const int K = 20;
   int seed = 377;
   int N = 1999911; // # of numbers generated
 
   test_poisson_distribution(N, K, seed);
+  test_geometric_distribution(N, K, seed);
+  test_random_distribution(N, K, seed);
+  test_merge(N, K, seed);
   return 0;
 }
 #endif
