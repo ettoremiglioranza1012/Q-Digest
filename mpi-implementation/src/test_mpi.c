a66a1b0f96373f8ed008fbc80e8a1f4834aadd2c
anthony-tricarico <tricarico672@gmail.com>
2025-11-22 15:25:58 +0100

    test: tried to understandwhy the MPI_Comm_split() would not return when number of processes is greater than 1

--- mpi-implementation/src/test_mpi.c
+++ mpi-implementation/src/test_mpi.c
@@ -1,67 +1,54 @@
-#include <mpi.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+#include "../../include/memory_utils.h"
 #include "../../include/qcore.h"
 #include "../include/tree_reduce.h"
-#include "../../include/memory_utils.h"
+#include <assert.h>
+#include <math.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define TEST 1
 
 int main(void) {
-    int rank, comm_sz;
+  int rank, comm_sz;
 
-    MPI_Init(NULL, NULL);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+  MPI_Init(NULL, NULL);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
 #if TEST
-    /* Exercise tree_reduce with deterministic QDigest */
-    int a[10] = {0,1,2,4,5,6,7,8,9};
-    int n = sizeof(a)/sizeof(a[0]);
-    
-    /* Handling possible rest division in n/comm_sz */
-    int base = n/comm_sz;
-    int rest = n % comm_sz;
-    int *counts = xmalloc(comm_sz*sizeof(int));
-    int *displs = xmalloc(comm_sz*sizeof(int));
-    int offset = 0;
+  /* Exercise tree_reduce with deterministic QDigest */
+  int a[10] = {0, 1, 2, 4, 5, 6, 7, 8, 9};
+  int n = sizeof(a) / sizeof(a[0]);
 
-    for (int i = 0; i < comm_sz; i++) {
-        counts[i] = base + (i < rest ? 1 : 0);
-        displs[i] = offset;
-        offset += counts[i];
-    }
+  /* Handling possible rest division in n/comm_sz */
+  int base = n / comm_sz;
+  int rest = n % comm_sz;
+  int *counts = xmalloc(comm_sz * sizeof(int));
+  int *displs = xmalloc(comm_sz * sizeof(int));
+  int offset = 0;
 
-    int local_n = counts[rank];
-    int *local_buf = xmalloc(local_n*sizeof(int));
+  for (int i = 0; i < comm_sz; i++) {
+    counts[i] = base + (i < rest ? 1 : 0);
+    displs[i] = offset;
+    offset += counts[i];
+  }
 
-    distribute_data_array(
-        a, 
-        local_buf,
-        counts,
-        displs,
-        local_n,
-        rank,
-        n,
-        1,
-        MPI_COMM_WORLD
-    );
-    size_t upper_bound = _get_curr_upper_bound(local_buf, local_n);
-    struct QDigest *q = _build_q_from_vector(local_buf, local_n, upper_bound);
-    tree_reduce(q, comm_sz, rank, MPI_COMM_WORLD);
+  int local_n = counts[rank];
+  int *local_buf = xmalloc(local_n * sizeof(int));
 
+  distribute_data_array(a, local_buf, counts, displs, local_n, rank, n, true,
+                        MPI_COMM_WORLD);
 
+  size_t upper_bound = _get_curr_upper_bound(local_buf, local_n);
+  struct QDigest *q = _build_q_from_vector(local_buf, local_n, upper_bound);
+  tree_reduce(q, comm_sz, rank, MPI_COMM_WORLD);
 
-    
 #endif
 
-    // test_tree_reduce() 
+  // test_tree_reduce()
 
-    MPI_Finalize();
-    printf("All test passed!\n");
-    return 0;
+  MPI_Finalize();
+  printf("All tests passed!\n");
+  return 0;
 }
-
-
--- mpi-implementation/src/tree_reduce.c
+++ mpi-implementation/src/tree_reduce.c
@@ -69,6 +69,7 @@ int *distribute_data_array(
                 0,          // root
                 comm
             );
+            printf("DEBUG (test): sent data to processes\n");
         } else {
             int *buf = xmalloc(buf_size*sizeof(int));
             _initialize_data_array(rank, buf, buf_size);
@@ -81,8 +82,9 @@ int *distribute_data_array(
                 local_n,    
                 MPI_INT,
                 0,          // root
-                MPI_COMM_WORLD
+                comm
             );
+            printf("DEBUG (test): sent data to processes\n");
             free(buf);
         }
     } else {
@@ -97,6 +99,7 @@ int *distribute_data_array(
             0,          // root
             MPI_COMM_WORLD
             );
+            printf("DEBUG (test): received data from root\n");
     }
     return local_buf;
 }   /* Read_vector */
@@ -108,16 +111,29 @@ void tree_reduce(
     int rank,
     MPI_Comm comm)
 {     
-    
+    // get comm size (number of processes - 1)
     int p = comm_size;
+    // variable to determine compacted number of processes
     int p2 = 1;
+    // increasing in powers of 2 to understand how 
+    // many processes are above the closest power of two
+    // of the current comm_size
     while (p2 * 2 <= p) p2 *= 2;
+    // the number of orphaned processes is determined by the difference
+    // of the total processes available in the communicator and
+    // the desired reduced size
     int orphans = p - p2;
-    
+
     /* REDUCE */
-    
+
     /* === Trim of the communicator === */
+
+    /* If orphans have been detected in the current communicator
+     * and their rank is in the specified range... */
     if (orphans > 0 && rank < 2 * orphans) { 
+        /* if their rank is odd send their serialized
+         * QDigest to the closest lower even-numbered process
+         * in the communicator */
         if (rank % 2) {
             size_t size = get_num_of_bytes(q);
             char *buf = xmalloc(size);
@@ -125,18 +141,23 @@ void tree_reduce(
             to_string(q, buf, &length);
             MPI_Send(&size, 1, MPI_UNSIGNED_LONG, rank-1, 0, comm);
             MPI_Send(buf, size, MPI_CHAR, rank-1, 0, comm);
+            printf("DEBUG (tree_reduce): rank %d sending data to rank %d\n", rank, rank-1);
             free(buf);
             delete_qdigest(q);
+            // return if rank is odd and was among those to be trimmed
+            // this effectively decreases the number of active processes
             return;
-        } else {
+        } else { // if even rank
             size_t recv_size;
             MPI_Recv(&recv_size, 1, MPI_UNSIGNED_LONG, rank + 1, 0, comm,
                 MPI_STATUS_IGNORE);
             char *buf = xmalloc(recv_size);
             MPI_Recv(buf, recv_size, MPI_CHAR, rank + 1, 0, comm,
                 MPI_STATUS_IGNORE);
+            printf("DEBUG (tree_reduce): rank %d receiving data from rank %d\n", rank, rank+1);
             struct QDigest *tmp = from_string(buf);
             merge(q, tmp);
+            printf("DEBUG (tree_reduce): after merging qdigest\n");
             delete_qdigest(tmp);
             free(buf);
         }
@@ -144,16 +165,38 @@ void tree_reduce(
     
     /* === Compact Communicator of survivors === */
     int is_orphan = ( (orphans > 0) && (rank < (2 * orphans)) && (rank % 2 != 0) ) ? MPI_UNDEFINED : 0;
+    printf("DEBUG (tree_reduce): right before comm split\n");
+    printf("DEBUG (tree_reduce): printing is_orphan: %d\n", is_orphan);
+
+    /* Why a barrier does not work here?
+     * Because the barrier will return only if all processes
+     * in a communicator have returned from its call.
+     * Here, when orphans are present, they might be dropped
+     * from the original communicator and therefore barrier will
+     * remain stuck.
+     * To be more convinced of this, try to uncomment the following
+     * 3 lines of code and run with an mpirun with a number
+     * of processes that is a power of 2 (e.g., 4, 8, etc.) */
+    // printf("DEBUG: before barrier\n");
+    // MPI_Barrier(comm);
+    // printf("DEBUG: after barrier\n");
+
     MPI_Comm tree_comm = MPI_COMM_NULL;
+    // FIXME: the program gets stuck here!
+    // The Comm_split does not appear to be returning from its call
     MPI_Comm_split(comm, is_orphan, rank, &tree_comm);
+    printf("DEBUG (tree_reduce): performed comm split\n");
     if (tree_comm == MPI_COMM_NULL) {
+        printf("DEBUG (tree_reduce): tree_comm is NULL\n");
         return;
     }
+    printf("DEBUG (tree_reduce): after return\n");
     
     int tree_rank, tree_size;
     MPI_Comm_rank(tree_comm, &tree_rank);
     MPI_Comm_size(tree_comm, &tree_size);
     
+    printf("DEBUG (tree_reduce): created trimmed communicator\n");
     /* === Power-of-two tree reduction === */
     int levels = log_2_ceil(tree_size);
     for (int k = 0; k < levels; k++) {
@@ -168,6 +211,8 @@ void tree_reduce(
             int receiver = tree_rank - step_size;
             MPI_Send(&size, 1, MPI_UNSIGNED_LONG, receiver, 0, tree_comm);
             MPI_Send(buf, size, MPI_CHAR, receiver, 0, tree_comm);
+            printf("DEBUG (tree_reduce): rank %d sent data to %d\n",
+                   tree_rank, receiver);
             free(buf);
             break;
         } else {
@@ -179,6 +224,8 @@ void tree_reduce(
             char *buf = xmalloc(recv_size);
             MPI_Recv(buf, recv_size, MPI_CHAR, sender, 0, tree_comm,
                 MPI_STATUS_IGNORE);
+            printf("DEBUG (tree_reduce): rank %d received data from %d\n",
+                   tree_rank, sender);
             struct QDigest *tmp = from_string(buf);
             merge(q, tmp);
             delete_qdigest(tmp);
